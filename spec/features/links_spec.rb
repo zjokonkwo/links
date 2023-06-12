b00d5471125beefb0a45a8aa0ac5b1266e50047f
Ben Purinton <ben.purinton@gmail.com>
2023-06-12 11:33:43 -0800

    added a few specs

---
+++ spec/features/links_spec.rb
@@ -0,0 +1,17 @@
+describe "The home page" do
+  it "renders an HTML page", points: 1 do
+    visit "/"
+
+    expect(page.status_code).to be(200)
+  end
+end
+
+describe "The home page" do
+  it "has at least three links", points: 3 do
+    visit "/"
+
+    a_count = all("a").count
+    expect(a_count).to be >= 3,
+                       "Expected page to have at least three <a> link elements, but found #{a_count} instead."
+  end
+end
