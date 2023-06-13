17fcd0c92e2915d952eb0ca8cce13fad0df20b44
Ben Purinton <ben.purinton@gmail.com>
2023-06-12 16:29:37 -0800

    added html validation specs (#1)

--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -7,11 +7,42 @@ describe "The home page" do
 end
 
 describe "The home page" do
-  it "has at least three links", points: 3 do
+  it "has at least three `<a>` link elements", points: 3 do
     visit "/"
 
     a_count = all("a").count
     expect(a_count).to be >= 3,
-                       "Expected page to have at least three 'a' HTML link elements, but found #{a_count} instead."
+      "Expected page to have at least three `<a>` HTML link elements, but found #{a_count} instead."
+  end
+end
+
+describe "The home page" do
+  it "has the DOCTYPE declaration as the VERY FIRST line.", points: 1 do
+    visit "/"
+    
+    source_code = page.html
+    source_code_without_extra_spaces = source_code.strip
+    first_line = source_code_without_extra_spaces.split("\n").first
+    downcased_first_line = first_line.downcase
+    expect(first_line).to match(/<!doctype html>/i),
+      "Expected the first line of the HTML file to contain a doctype declaration. Found: #{first_line}"
+  end
+end
+
+describe "The home page" do
+  it "has a `<head>` section to make it a valid HTML document.", points: 1 do
+    visit "/"
+
+    expect(page).to have_tag("head", count: 1),
+      "Expected page to have a `<head>` HTML element to make it a valid HTML document."
+  end
+end
+
+describe "The home page" do
+  it "has a `<body>` section to make it a valid HTML document.", points: 1 do
+    visit "/"
+
+    expect(page).to have_tag("body", count: 1),
+      "Expected page to have a `<body>` HTML element to make it a valid HTML document."
   end
 end
